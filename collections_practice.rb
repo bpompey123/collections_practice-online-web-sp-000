4b5081591eae636caa876c45ce37a2e6b9f50f05
Brian Pompey <brianthescholar@gmail.com>
2019-08-03 22:06:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,7 +13,8 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-
+  collection = []
+  collection << array[0] + array[2] + array[1] + array[3..-1]
 end
 
 def reverse_array(array)
