b0085bea2b35b293d6991bdd9d2b495712aacb24
Brian Pompey <brianthescholar@gmail.com>
2019-08-03 21:35:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+    array.sort_by(&:length)
+end
+
+def swap_elements(array)
+  
+end
